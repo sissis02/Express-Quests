b9e24363b8f28bd7a072dc05ffc056c6f9474228
sissis02 <sissis1993@gmail.com>
2023-11-27 13:35:41 +0100

    lecture depuis la base de donnees

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+
+const port = process.env.APP_PORT;
 
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+
+const userControllers = require("./controllers/userControllers"); 
+
+app.get("/api/users", userControllers.getUsers); 
+app.get("/api/users/:id", userControllers.getUserById); 
 
 module.exports = app;
